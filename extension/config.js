e66b2827549f4b7db5f581041d29a6d43f3293e7
Kunalkhuteta <kunalkhuteta@gmail.com>
2026-02-25 22:57:27 +0530

    update config

--- extension/config.js
+++ extension/config.js
@@ -2,7 +2,7 @@
 const RENDER_URL = "https://habit-tracker-extension.onrender.com";
 
 // Set to true since your server is deployed on Render
-const IS_PRODUCTION = false;
+const IS_PRODUCTION = true;
 
 const PROD_API_URL = RENDER_URL;
 const DEV_API_URL  = "http://localhost:5000";
